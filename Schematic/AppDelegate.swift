2aae12abeaa799db128f3b457c48e215d545f392
Matt Brandt <mkbrandt@mac.com>
2016-05-13 15:26:33 -0500

    Initial Commit

---
+++ Schematic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Schematic
+//
+//  Created by Matt Brandt on 5/13/16.
+//  Copyright © 2016 Walkingdog. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
